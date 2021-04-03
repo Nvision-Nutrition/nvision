41420999ad788d881a8558719d28f1cba5c93179
Emma <knoremma@gmaill.com>
2021-04-03 11:46:17 -0600

    Add db queries for inserting calories & water and getting a success and failure quote

--- db/index.js
+++ db/index.js
@@ -29,7 +29,72 @@ const sampleQuery = (req, res) => {
       });
 };
 
+// insert into entries - calorie record
+const insertCalories = (req, res) => {
+  const { userId, type, calories, mealName } = req.body;
+  const queryString = `INSERT INTO entries(type, calories, mealName, date, user_id)
+                       VALUES($1, $2, $3, current_timestamp, $4)';`,
+                       [type, calories, mealName, userId];
+  pool.query(queryString)
+      .then((response) => {
+        res.status(201).send('Calorie entry successful!');
+      }).catch((err) => {
+        console.error(err);
+        res.status(400).send(err);
+      });
+}
+
+// insert into entries - water record
+const insertWater = (req, res) => {
+  const { userId, type, water, mealName } = req.body;
+  const queryString = `INSERT INTO entries(type, water, mealName, date, user_id)
+                       VALUES($1, $2, $3, current_timestamp, $4)';`,
+                       [type, water, mealName, userId];
+  pool.query(queryString)
+      .then((response) => {
+        res.status(201).send('Water entry successful!');
+      }).catch((err) => {
+        console.error(err);
+        res.status(400).send(err);
+      });
+}
+
+// get a random fail quote
+const getFail = (req, res) => {
+  const queryString = 'SELECT failure_quote FROM quotes ORDER BY RANDOM() LIMIT 1';
+
+  pool.query(queryString)
+      .then((fail_quote) => {
+        res.status(200).send(fail_quote.rows);
+      }).catch((err) => {
+        console.error(err);
+        res.status(404).send(err);
+      });
+}
+
+// get a random success quote
+const getSuccess = (req, res) => {
+  const queryString = 'SELECT success_quote FROM quotes ORDER BY RANDOM() LIMIT 1';
+
+  pool.query(queryString)
+      .then((success_quote) => {
+        res.status(200).send(success_quote.rows);
+      }).catch((err) => {
+        console.error(err);
+        res.status(404).send(err);
+      });
+  }
+
+
+
 module.exports = {
   // put database query function exports here
   sampleQuery,
+  insertCalories,
+  insertWater,
+  getSuccess,
+  getFail,
 };
+
+
+'SELECT failure_quote FROM quotes ORDER BY RANDOM() LIMIT 1'
