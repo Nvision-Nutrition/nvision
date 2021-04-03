3c8b0d6382767d700442344eff916274eec9dc92
Scott Guinn <guinnscott111@gmail.com>
2021-04-03 12:08:05 -0600

    fetchWeek implemented using sumDay helper query func.

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
@@ -18,15 +18,15 @@ const sampleQuery = (req, res) => {
                        WHERE user_id='${userID}';`;
 
   pool.query(queryString)
-      .then((response) => {
-        // do stuff with the response from db here
+    .then((response) => {
+      // do stuff with the response from db here
 
-        // send nicely formatted data back to the client here:
-        res.send(response);
-      }).catch((err) => {
-        console.error(err);
-        res.sendStatus(500);
-      });
+      // send nicely formatted data back to the client here:
+      res.send(response);
+    }).catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 // Fetches the total calorie and water count for a given date and user_id
@@ -36,13 +36,37 @@ const fetchDayCount = (req, res) => {
 
   // 'userID' defaults to 1 for testing purposes only
   // 'date' defaults to today's date (Format: "2021-04-03")
-  const {userID = 1, date = new Date().toISOString().slice(0, 10)} = req.query;
+  const { userID = 1, date = new Date().toISOString().slice(0, 10) } = req.query;
 
   const queryString = `SELECT calories, water
                        FROM entries
                        WHERE user_id=${userID} AND date='${date}';`;
 
   pool.query(queryString)
+    .then((response) => {
+      let calorieSum = 0;
+      let waterSum = 0;
+
+      response.rows.forEach((entry) => {
+        calorieSum += entry.calories;
+        waterSum += entry.water;
+      });
+
+      res.send({ calorieSum: calorieSum, waterSum: waterSum });
+    }).catch((err) => {
+      console.error(err);
+      res.status(500).send();
+    });
+};
+
+const sumDay = (userID, date) => {
+  const queryString = `SELECT calories, water
+                       FROM entries
+                       WHERE user_id=${userID} AND date='${date}';`;
+
+  return new Promise((resolve, reject) => {
+
+    pool.query(queryString)
       .then((response) => {
         let calorieSum = 0;
         let waterSum = 0;
@@ -51,16 +75,55 @@ const fetchDayCount = (req, res) => {
           calorieSum += entry.calories;
           waterSum += entry.water;
         });
+        const sums = { calorieSum: calorieSum, waterSum: waterSum }
+        resolve(sums);
+      }).catch((err) => reject(err));
 
-        res.send({calorieSum: calorieSum, waterSum: waterSum});
-      }).catch((err) => {
-        console.error(err);
-        res.status(500).send();
-      });
-};
+  })
+}
+
+/*
+    fetchWeek returns calorie and water sums for the past seven days in the following format:
+  [
+    { 2021-04-03: {
+                    calorieCount: 2000,
+                    waterCount: 86
+                  },
+      2021-04-02: {
+                    calorieCount: 1800,
+                    waterCount: 76
+                  }
+  ]
+  */
+
+const fetchWeek = async (req, res) => {
+  try {
+    const { userID = 1 } = req.query;
+
+    let week = [];
+
+    let today = new Date();
+    // iterate through the past seven days
+    for (let i = 0; i < 7; i++) {
+      let currentDay = {};
+      let date = new Date(today);
+      date.setDate(date.getDate() - i);
+      const formattedDate = date.toISOString().slice(0, 10);
+      currentDay[formattedDate] = await sumDay(userID, formattedDate);
+      week.push(currentDay);
+    }
+
+    res.send(week);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+
+}
 
 module.exports = {
   // put database query function exports here
   sampleQuery,
   fetchDayCount,
+  fetchWeek
 };
--- pages/api/progress.js
+++ pages/api/progress.js
@@ -1,7 +1,11 @@
 const queries = require('../../db/index.js');
 
 export default (req, res) => {
-  queries.fetchDayCount(req, res);
+  if (req.query.type === 'day') {
+    queries.fetchDayCount(req, res);
+  } else if (req.query.type === 'week') {
+    queries.fetchWeek(req, res);
+  }
 };
 
 // disables the warning for an unresolved api request
@@ -10,3 +14,4 @@ export const config = {
     externalResolver: true,
   },
 };
+
