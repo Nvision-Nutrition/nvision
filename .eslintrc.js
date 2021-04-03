82c6154f70c3131a5d5e1c38836021d2a120ab11
Taylor Smart <75653507+taylorsmart@users.noreply.github.com>
2021-04-03 14:58:21 -0600

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,13 @@
 module.exports = {
-<<<<<<< HEAD
-  'env': {
-    'browser': true,
-    'es2021': true,
-=======
-<<<<<<< HEAD
   'env': {
     'browser': true,
     'es6': true,
->>>>>>> 99a47202d581dda5cc382a2d470438834704bb2b
     'node': true,
   },
   'extends': [
     'plugin:react/recommended',
     'google',
   ],
-<<<<<<< HEAD
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
-=======
   'globals': {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly',
@@ -45,28 +24,4 @@ module.exports = {
   ],
   'rules': {
   },
-=======
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
->>>>>>> ea34e3415b0dfc493dbe9e081dcc66ee95b3cf4c
->>>>>>> 99a47202d581dda5cc382a2d470438834704bb2b
 };
