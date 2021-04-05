2f7776f875328b0dec2edf93342ee9dad68b62c2
Mauricio Moreno <Mmoren01@Mauricios-MBP13.local>
2021-04-05 16:23:11 -0700

    Cleaned up some ESlint issues

--- components/CalorieTracker.jsx
+++ components/CalorieTracker.jsx
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Apple from '../public/appleStencil.svg';
 
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const CalorieTracker = () => {
-  const [progress, setProgress] = useState(0);
+  const [progress] = useState(0);
 
   return (
     <>
