dd377930e8eb0cabdd7fe59c45fd2d618c66d539
Taylor Smart <taylorsmart@Taylors-MacBook-Pro.local>
2021-04-05 14:29:44 -0600

    linter adjustments to waterDaily.jsx for comment stepping

--- components/waterDaily.jsx
+++ components/waterDaily.jsx
@@ -4,15 +4,19 @@ import styles from '../styles/Home.module.css';
 
 const WaterDaily = () => {
   const generateWaterGraph = (waterDrank = 0, waterGoal = 100) => {
-    
-    // This finds the percentage rounded to the tens digit 62 => 60 => 6, representing 6 crushed water bottles out of 10
+    /*
+     This finds the percentage rounded to the tens digit 62 => 60 => 6,
+      representing 6 crushed water bottles out of 10
+    */
     const waterPercentage = waterGoal > 0 ?
       Math.round((waterDrank / waterGoal) * 10) :
       0;
 
     const bottleArray = [];
     for (let j = 0; j < 10; j += 1) {
-      // Iteration of 6/10 walk through - waterPercentage === 6 - seeking to add 6 crushed bottles, and four full
+      /* Iteration of 6/10 walk through - waterPercentage === 6 -
+         seeking to add 6 crushed bottles, and four full
+      */
       switch (waterPercentage) {
         case 0: // waterPercentage = 6 - will not add a full bottle
           bottleArray.push(
@@ -23,24 +27,24 @@ const WaterDaily = () => {
                   className="bottle-image" />
               </div>);
           break;
-         // case 10: // waterPercentage = 6 - will not add a full bottle
-         // bottleArray.push(
-         //     <div key={uuidv4()}>
-         //       <img
-         //         src="/massive_celebration_waterfall.gif"
-         //         alt=""
-         //         className="bottle-image" />
-         //     </div>);
-         // break;
-         // case 1: // waterPercentage = 6 - will not add a full bottle
-         // bottleArray.push(
-         //     <div key={uuidv4()}>
-         //       <img
-         //         src="/fancy_crushed_bottle.jpg"
-         //         alt=""
-         //         className="bottle-image" />
-         //     </div>);
-         // break;
+          // case 10: // waterPercentage = 6 - will not add a full bottle
+          // bottleArray.push(
+          //     <div key={uuidv4()}>
+          //       <img
+          //         src="/massive_celebration_waterfall.gif"
+          //         alt=""
+          //         className="bottle-image" />
+          //     </div>);
+          // break;
+          // case 1: // waterPercentage = 6 - will not add a full bottle
+          // bottleArray.push(
+          //     <div key={uuidv4()}>
+          //       <img
+          //         src="/fancy_crushed_bottle.jpg"
+          //         alt=""
+          //         className="bottle-image" />
+          //     </div>);
+          // break;
         default: // waterPercentage = 6 - execute default and add crushed bottle
           bottleArray.push(
               <div key={uuidv4()}>
@@ -50,11 +54,11 @@ const WaterDaily = () => {
                   className="bottle-image" />
               </div>);
           /*
-            waterPercentage decremented 1 to 5.  
-            Once all "drank" percentage points have run out, 
+            waterPercentage decremented 1 to 5.
+            Once all "drank" percentage points have run out,
             full bottles will be added to the result array.
           */
-          waterPercentage -= 1; 
+          waterPercentage -= 1;
           break;
       }
     }
@@ -70,7 +74,7 @@ const WaterDaily = () => {
 
   return (
     <>
-      <div className={`${styles.card} water-history chart`}>
+      <div className={`${styles.card} water-daily chart`}>
         <p className={`${styles.description} subtitle`}>{`Water Intake`}</p>
         { generateWaterGraph() }
       </div>
