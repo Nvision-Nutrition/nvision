74908bb5ae61ed2b24f96781e19ed594661f1df8
Scott Guinn <guinnscott111@gmail.com>
2021-04-06 16:40:43 -0600

    refactored GlobalStateProvider wrapper

--- _tests_/front-end/components/nvisionNavbar.test.js
+++ _tests_/front-end/components/nvisionNavbar.test.js
@@ -5,40 +5,43 @@ import * as React from 'react';
 import {GlobalStateProvider} from 'globalState.js';
 import NvisionNavbar from 'nvisionNavbar.jsx';
 
+describe.only('Nvision Nabar testing suite', () => {
+  const renderComponent = () => {
+    // this wrapper argument wraps the NvisionNavbar component in the
+    // GlobalStateProvider context.
+    return render(<NvisionNavbar/>, {wrapper: GlobalStateProvider});
+  };
 
-const renderComponent = () => {
-  return render(<GlobalStateProvider><NvisionNavbar/></GlobalStateProvider>);
-};
+  it('should render the logo', () => {
+    const {queryByAltText} = renderComponent();
+    const logo = queryByAltText('nVision logo');
+    expect(logo).toBeTruthy();
+  });
 
-it('should render the logo', () => {
-  const {queryByAltText} = renderComponent();
-  const logo = queryByAltText('nVision logo');
-  expect(logo).toBeTruthy();
-});
+  it('should render a dark mode button', () => {
+    const {queryByLabelText} = renderComponent();
+    const darkMode = queryByLabelText('dark-mode-toggle');
+    expect(darkMode).toBeTruthy();
+  });
 
-it('should render a dark mode button', () => {
-  const {queryByLabelText} = renderComponent();
-  const darkMode = queryByLabelText('dark-mode-toggle');
-  expect(darkMode).toBeTruthy();
-});
+  it('should render the text "Sign Out" ', () => {
+    const {queryByText} = renderComponent();
+    const signOut = queryByText('Sign Out');
+    expect(signOut).toBeTruthy();
+  });
 
-it('should render the text "Sign Out" ', () => {
-  const {queryByText} = renderComponent();
-  const signOut = queryByText('Sign Out');
-  expect(signOut).toBeTruthy();
-});
+  it('toggles dark mode button on click', () => {
+    const {queryByLabelText} = renderComponent();
+    const darkMode = queryByLabelText('dark-mode-toggle');
 
-it('toggles dark mode button on click', () => {
-  const {queryByLabelText} = renderComponent();
-  const darkMode = queryByLabelText('dark-mode-toggle');
+    expect(darkMode).toBeTruthy();
+    expect(darkMode).toHaveClass('btn-outline-dark');
 
-  expect(darkMode).toBeTruthy();
-  expect(darkMode).toHaveClass('btn-outline-dark');
+    userEvent.click(darkMode);
+    expect(darkMode).toHaveClass('btn-outline-light');
 
-  userEvent.click(darkMode);
-  expect(darkMode).toHaveClass('btn-outline-light');
-
-  userEvent.click(darkMode);
-  expect(darkMode).toHaveClass('btn-outline-dark');
+    userEvent.click(darkMode);
+    expect(darkMode).toHaveClass('btn-outline-dark');
+  });
 });
 
