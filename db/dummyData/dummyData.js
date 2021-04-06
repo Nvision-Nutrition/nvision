0966b168707e9908417141ce2c0760b6a054144c
Taylor Smart <taylorsmart@Taylors-MacBook-Pro.local>
2021-04-05 22:31:44 -0600

    adding dummy data to avoid error on fetch

---
+++ db/dummyData/dummyData.js
@@ -0,0 +1,53 @@
+const sevenDayData = [
+  {
+    '2021-04-06': {
+      'calorieSum': 2458,
+      'waterSum': 3,
+      'weightSum': 245,
+    },
+  },
+  {
+    '2021-04-05': {
+      'calorieSum': 3214,
+      'waterSum': 6,
+      'weightSum': 243,
+    },
+  },
+  {
+    '2021-04-04': {
+      'calorieSum': 3541,
+      'waterSum': 1,
+      'weightSum': 242,
+    },
+  },
+  {
+    '2021-04-03': {
+      'calorieSum': 2548,
+      'waterSum': 2,
+      'weightSum': 240,
+    },
+  },
+  {
+    '2021-04-02': {
+      'calorieSum': 2846,
+      'waterSum': 0,
+      'weightSum': 238,
+    },
+  },
+  {
+    '2021-04-01': {
+      'calorieSum': 1994,
+      'waterSum': 6,
+      'weightSum': 240,
+    },
+  },
+  {
+    '2021-03-31': {
+      'calorieSum': 3124,
+      'waterSum': 8,
+      'weightSum': 241,
+    },
+  },
+];
+
+export default sevenDayData;
