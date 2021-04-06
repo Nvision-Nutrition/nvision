a4c2e47e257d8c5a89b67e817281e8d99a7e3f81
Taylor Smart <taylorsmart@Taylors-MacBook-Pro.local>
2021-04-05 21:59:43 -0600

    Completed generic build out of graph with toggle and weight capabilities

--- components/historyGraph.jsx
+++ components/historyGraph.jsx
@@ -1,42 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ResponsiveBar} from '@nivo/bar';
-
+import sevenDayFetch from '../db/dummyData/dummyData.js';
 import styles from '../styles/Home.module.css';
+import {Button} from 'react-bootstrap';
 
 const HistoryGraph = () => {
-  const goalValue = 3000;
-  const keyValue = 'Calories'
+  // Macro Number 0 = cal, 1 = water, 2 = weight
+  const [macroNumber, setMacroNumber] = useState(1);
 
-  const data = [
-    {
-      'dow': 'Sun',
-      [keyValue]: 3053,
-    },
-    {
-      'dow': 'Mon',
-      [keyValue]: 3003,
-    },
-    {
-      'dow': 'Tue',
-      [keyValue]: 2758,
-    },
-    {
-      'dow': 'Wed',
-      [keyValue]: 2893,
-    },
-    {
-      'dow': 'Thu',
-      [keyValue]: 4208,
-    },
-    {
-      'dow': 'Fri',
-      [keyValue]: 3256,
-    },
-    {
-      'dow': 'Sat',
-      [keyValue]: 2784,
-    },
-  ];
+  // keyValue: Graph set up/ aesthetics
+  // fetchValue: Define the appropriate db column to fetch from
+  // goalValue: User defined goal
+  // buttonName: Aesthetic control for Button Toggle
+  let [keyValue, fetchValue, goalValue, buttonName] =
+    ['Calories', 'calorieSum', 3000, 'Check Water History'];
+
+  switch (macroNumber) {
+    case 1:
+      keyValue = 'Water';
+      fetchValue = 'waterSum';
+      goalValue = 5;
+      buttonName = 'Check Weight History';
+      break;
+    case 2:
+      keyValue = 'Weight';
+      fetchValue = 'weightSum';
+      goalValue = 180;
+      buttonName = 'Check Calorie History';
+      break;
+    default:
+      keyValue = 'Calories';
+      fetchValue = 'calorieSum';
+      goalValue = 3000;
+      buttonName = 'Check Water History';
+      break;
+  }
+
+
+  // Fetch and format data appropriately
+  // NOTE:
+  //   This will fetch the rows of data that are available from the last seven
+  //   day fetch.  If no data was input, the days may be skipped.
+  const data = [];
+  Object.keys(sevenDayFetch).forEach((day) => {
+    // Fetch date key
+    const date = Object.keys(sevenDayFetch[day]);
+    // Re-format key to dow in short form (Mon, Tues, Wed)
+    const dow = new Date(date).toLocaleString('en-us', {weekday: 'short'});
+    // Fetch the appropriate value for the date in question
+    const val = sevenDayFetch[day][date][fetchValue];
+
+    // Add object to data array
+    data.push({
+      'dow': dow,
+      [keyValue]: val,
+    });
+  });
+
+
+  // Button Control
+  // Switches between the different macro values
+  const graphSwap = () => {
+    setMacroNumber(macroNumber === 2 ?
+      0 :
+      (macroNumber + 1 ));
+  };
+
+  /*
+    Config information for the graph to render appropriately and look nice
+  */
 
   const config = {
     keys: [
@@ -49,34 +81,16 @@ const HistoryGraph = () => {
       'left': 60,
     },
     defs: [
-      {
-        'id': 'dots',
-        'type': 'patternDots',
-        'background': 'inherit',
-        'color': '#38bcb2',
-        'size': 4,
-        'padding': 1,
-        'stagger': true,
-      },
-      {
-        'id': 'lines',
-        'type': 'patternLines',
-        'background': 'inherit',
-        'color': '#eed312',
-        'rotation': -45,
-        'lineWidth': 6,
-        'spacing': 10,
-      },
     ],
     fill: [
     ],
     axisBottom: {
       'tickSize': 5,
       'tickPadding': 5,
-      'tickRotation': 0,
+      'tickRotation': 90,
       'legend': 'Day of Week',
       'legendPosition': 'middle',
-      'legendOffset': 32,
+      'legendOffset': 38,
     },
     axisLeft: {
       'tickSize': 5,
@@ -84,7 +98,7 @@ const HistoryGraph = () => {
       'tickRotation': 0,
       'legend': `${keyValue}`,
       'legendPosition': 'middle',
-      'legendOffset': -40,
+      'legendOffset': -41,
     },
     legends: [
       {
@@ -105,18 +119,18 @@ const HistoryGraph = () => {
   return (
     <>
       <div className={`${styles.card} history-graph`}>
-        <p className={`${styles.description} subtitle`}>{`Last 7-Days`}</p>
+        <p className={`${styles.description} subtitle`}>{`${keyValue}`}
+        </p>
         <div className="chart">
           <ResponsiveBar
             data={data}
             keys={config.keys}
+            enableLabel={false}
             indexBy="dow"
             margin={config.margin}
             padding={0.3}
             colors={{scheme: 'category10'}}
             colorBy="id"
-            defs={config.defs}
-            fill={config.fill}
             borderColor="inherit:darker(1.6)"
             axisTop={null}
             axisRight={null}
@@ -130,15 +144,18 @@ const HistoryGraph = () => {
             motionDamping={15}
             markers={[
               {
-                  axis: 'y',
-                  value: goalValue,
-                  lineStyle: { stroke: 'rgba(0, 0, 0, .35)', strokeWidth: 2 },
-                  legend: `Target ${goalValue}`,
-                  legendOrientation: 'vertical',
+                axis: 'y',
+                value: goalValue,
+                lineStyle: {stroke: 'rgba(0, 0, 0, .35)', strokeWidth: 2},
+                // legend: `Target ${goalValue}`,
+                // legendOrientation: 'horizontal',
               },
             ]}
           />
         </div>
+        <Button variant="outline-secondary" onClick={graphSwap}>
+          {buttonName}
+        </Button>
       </div>
     </>
   );
