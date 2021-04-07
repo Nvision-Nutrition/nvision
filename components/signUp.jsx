f6f0ed62e87ec331d720c3309d782c26064b3c16
Scott Guinn <75865235+Scott-Guinn@users.noreply.github.com>
2021-04-07 12:40:16 -0600

    Removed comments

--- components/signUp.jsx
+++ components/signUp.jsx
@@ -196,66 +196,6 @@ const SignUp = () => {
         </Button>
       </Form>
     </Container>
-    // <div id="signUpDiv" style={{'textAlign': 'center'}}>
-    //   <h1>Sign Up</h1>
-    //   <form>
-    //     <label>First Name</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Last Name</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Username</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Password</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Password again</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Calorie Goal</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Water Goal (ounces)</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Weight Goal</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Phone number</label>
-    //     <br />
-    //     <input type="number"></input>
-    //     <br />
-    //     <label>Email</label>
-    //     <br />
-    //     <input type="text"></input>
-    //     <br />
-    //     <label>Sex(assigned at birth)</label>
-    //     <br />
-    //     <select style={{'width': '100%'}}>
-    //       <option>Male</option>
-    //       <option>Female</option>
-    //       <option>Choose not to disclose</option>
-    //     </select>
-    //     <br />
-    //     <br />
-    //     <input
-    //       type="submit"
-    //       style={{'width': '100%', 'height': '50px'}} >
-    //     </input>
-    //   </form>
-    //   <br />
-    //   <button style={{'width': '100%', 'height': '50px'}}>Login</button>
-    // </div>
   );
 };
 
