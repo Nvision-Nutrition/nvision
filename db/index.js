950ea5db58ad7a79e03c57b9302c30dfb51ccd35
Scott Guinn <guinnscott111@gmail.com>
2021-04-08 13:59:37 -0600

    removed pool.end invocations

--- db/index.js
+++ db/index.js
@@ -61,11 +61,9 @@ const fetchDayCount = async (req, res) => {
     const day = {};
     day[date] = await sumDay(userID, date);
     res.send(day);
-    pool.end();
   } catch (err) {
     console.error(err);
     res.status(500).send();
-    pool.end();
   }
 };
 
@@ -100,11 +98,9 @@ const fetchWeek = async (req, res) => {
     }
 
     res.send(week);
-    pool.end();
   } catch (err) {
     console.error(err);
     res.status(500).send();
-    pool.end();
   }
 };
 
@@ -117,11 +113,9 @@ const insertCalories = (req, res) => {
   pool.query(queryString, [mealType, calories, mealName, usersDate, userId])
       .then((response) => {
         res.status(201).send('Calorie entry successful!');
-        pool.end();
       }).catch((err) => {
         console.error(err);
         res.status(400).send(err);
-        pool.end();
       });
 };
 
@@ -172,7 +166,6 @@ const addUser = async (req, res) => {
     if (userID !== -1) {
       // user exists already
       res.status(501).send(`user exists already with userID: ${userID}`);
-      pool.end();
     } else {
       // create a new user
       const queryString = `INSERT INTO users(
@@ -190,13 +183,11 @@ const addUser = async (req, res) => {
           }).catch((err) => {
             console.error(err);
             res.send(500);
-            pool.end();
           });
     }
   } catch (err) {
     console.error(err);
     res.status(500).send();
-    pool.end();
   }
 };
 
@@ -226,11 +217,9 @@ const insertWater = (req, res) => {
   pool.query(queryString, [waterType, water, usersDate, userId])
       .then((response) => {
         res.status(201).send('Water entry successful!');
-        pool.end();
       }).catch((err) => {
         console.error(err);
         res.status(400).send(err);
-        pool.end();
       });
 };
 
@@ -242,11 +231,9 @@ const insertWeight = (req, res) => {
   pool.query(queryString, [type, weight, usersDate, userId])
       .then((response) => {
         res.status(201).send('Weight entry successful!');
-        pool.end();
       }).catch((err) => {
         console.error(err);
         res.status(400).send(err);
-        pool.end();
       });
 };
 
@@ -258,11 +245,9 @@ const getFail = (req, res) => {
   pool.query(queryString)
       .then((failQuote) => {
         res.status(200).send(failQuote.rows);
-        pool.end();
       }).catch((err) => {
         console.error(err);
         res.status(404).send(err);
-        pool.end();
       });
 };
 
@@ -274,11 +259,9 @@ const getSuccess = (req, res) => {
   pool.query(queryString)
       .then((successQuote) => {
         res.status(200).send(successQuote.rows);
-        pool.end();
       }).catch((err) => {
         console.error(err);
         res.status(404).send(err);
-        pool.end();
       });
 };
 
